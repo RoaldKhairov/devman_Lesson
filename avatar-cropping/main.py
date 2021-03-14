89838dbbc4fd75fa6076f104929c6054c6affcb8
RoaldKhairov <59236211+RoaldKhairov@users.noreply.github.com>
2021-03-14 21:15:12 +0300

    Add files via upload

---
+++ avatar-cropping/main.py
@@ -0,0 +1,25 @@
+from PIL import Image
+
+image = Image.open("example.jpg")
+image = image.convert("RGB")
+
+red, green, blue = image.split()
+
+offset = 50
+
+cropped_middle_coordinates = (offset/2, 0, (image.width - offset/2), image.height)
+   
+cropped_left_red = red.crop((offset, 0, red.width, red.height)) 
+cropped_middle_red = red.crop(cropped_middle_coordinates) 
+blended_red = Image.blend(cropped_left_red, cropped_middle_red,  0.2)
+
+cropped_right_blue = blue.crop((0, 0,(blue.width - offset), blue.height)) 
+cropped_middle_blue = blue.crop(cropped_middle_coordinates) 
+blended_blue = Image.blend(cropped_right_blue, cropped_middle_blue, 0.2)
+
+middle_green = green.crop(cropped_middle_coordinates)
+
+final_image = Image.merge("RGB", (blended_red, blended_blue, middle_green))
+final_image.thumbnail((80, 80))
+final_image.save('final_image.jpg')
+
